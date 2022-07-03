b01a12f828426c78ef821a7904613b74de19fe15
EtoDemerzel <huangweiran1998@gmail.com>
2022-07-03 18:36:27 -0500

    update ddp output

--- ddp/ddp_trainer.py
+++ ddp/ddp_trainer.py
@@ -3,6 +3,7 @@ from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
+import resnet
 
 import torchvision
 import torchvision.transforms as transforms
@@ -11,44 +12,179 @@ import argparse
 import os
 import random
 import numpy as np
+import time
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 def set_random_seeds(random_seed=0):
     torch.manual_seed(random_seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.benchmark = True
     np.random.seed(random_seed)
     random.seed(random_seed)
 
+def train(train_loader, model, device, criterion, optimizer, epoch):
+    """
+        Run one train epoch
+    """
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
 
-def evaluate(model, device, test_loader):
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        target = target.to(device)
+        input_var = input.to(device)
+        target_var = target
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        output = output.float()
+        loss = loss.float()
+        # measure accuracy and record loss
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 50 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                epoch, i, len(train_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1))
+
+
+def evaluate(model, device, test_loader, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
     model.eval()
 
-    correct = 0
-    total = 0
+    end = time.time()
     with torch.no_grad():
-        for data in test_loader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        for i, (input, target) in enumerate(test_loader):
+            target = target.to(device)
+            input_var = input.to(device)
+            target_var = target
 
-    accuracy = correct / total
+            # compute output
+            output = model(input_var)
+            loss = criterion(output, target_var)
+
+            output = output.float()
+            loss = loss.float()
+
+            # measure accuracy and record loss
+            prec1 = accuracy(output.data, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % 50 == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                    i, len(test_loader), batch_time=batch_time, loss=losses,
+                    top1=top1))
+
+    print(' * Prec@1 {top1.avg:.3f}'
+          .format(top1=top1))
+
+    # correct = 0
+    # total = 0
+    # with torch.no_grad():
+    #     for i, data in enumerate(test_loader):
+    #         images, labels = data[0].to(device), data[1].to(device)
+    #         outputs = model(images)
+    #         _, predicted = torch.max(outputs.data, 1)
+    #         total += labels.size(0)
+    #         correct += (predicted == labels).sum().item()
+    #
+    # accuracy = correct / total
 
     return accuracy
 
+best_prec1 = 0
 
 def main():
-    num_epochs_default = 10000
-    batch_size_default = 256  # 1024
+    global best_prec1
+    num_epochs_default = 200 #10000
+    batch_size_default = 128  # 1024
     learning_rate_default = 0.1
     random_seed_default = 0
     model_dir_default = "saved_models"
     model_filename_default = "resnet_distributed.pth"
 
+    model_names = sorted(name for name in resnet.__dict__
+                         if name.islower() and not name.startswith("__")
+                         and name.startswith("resnet")
+                         and callable(resnet.__dict__[name]))
+
+    print(model_names)
+
     # Each process runs on 1 GPU device specified by the local_rank argument.
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet32',
+                        choices=model_names,
+                        help='model architecture: ' + ' | '.join(model_names) +
+                             ' (default: resnet32)')
     parser.add_argument("--local_rank", type=int,
                         help="Local rank. Necessary for using the torch.distributed.launch utility.")
     parser.add_argument("--num_epochs", type=int, help="Number of training epochs.", default=num_epochs_default)
@@ -87,7 +223,8 @@ def main():
     # torch.distributed.init_process_group(backend="gloo")
 
     # Encapsulate the model on the GPU assigned to the current process
-    model = torchvision.models.resnet18(pretrained=False)
+    # model = torchvision.models.resnet18(pretrained=False)
+    model = resnet.__dict__[argv.arch]()
 
     device = torch.device("cuda:{}".format(local_rank))
     model = model.to(device)
@@ -100,17 +237,23 @@ def main():
         ddp_model.load_state_dict(torch.load(model_filepath, map_location=map_location))
 
     # Prepare dataset and dataloader
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
     transform = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        normalize, #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
     # Data should be prefetched
     # Download should be set to be False, because it is not multiprocess safe
     train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=False, transform=transform)
-    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=False, transform=transform)
+    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ]))
 
     # Restricts data loading to a subset of the dataset exclusive to the current process
     train_sampler = DistributedSampler(dataset=train_set)
@@ -120,7 +263,9 @@ def main():
     test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=8)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-4)
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                        milestones=[100, 150], last_epoch= -1)
 
     # Loop over the dataset multiple times
     for epoch in range(num_epochs):
@@ -130,21 +275,28 @@ def main():
         # Save and evaluate model routinely
         if epoch % 10 == 0:
             if local_rank == 0:
-                accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                prec1 = evaluate(model=ddp_model, device=device, test_loader=test_loader, criterion=criterion)
+
+                # # remember best prec@1 and save checkpoint
+                # is_best = prec1 > best_prec1
+                # best_prec1 = max(prec1, best_prec1)
                 torch.save(ddp_model.state_dict(), model_filepath)
-                print("-" * 75)
-                print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
-                print("-" * 75)
+                # print("-" * 75)
+                # print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
+                # print("-" * 75)
 
         ddp_model.train()
+        train(train_loader, model, device, criterion, optimizer, epoch)
+        lr_scheduler.step()
 
-        for data in train_loader:
-            inputs, labels = data[0].to(device), data[1].to(device)
-            optimizer.zero_grad()
-            outputs = ddp_model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+        # for data in train_loader:
+        #     inputs, labels = data[0].to(device), data[1].to(device)
+        #     optimizer.zero_grad()
+        #     outputs = ddp_model(inputs)
+        #     loss = criterion(outputs, labels)
+        #     loss.backward()
+        #     optimizer.step()
+        #     lr_scheduler.step()
 
 
 if __name__ == "__main__":
